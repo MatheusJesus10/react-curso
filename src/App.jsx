d4f2dc600d4c566aacaaf9f31cf42e790c8f35b6
Matheus Jesus <MatheusJesusJesus07@hotmail.com>
2025-04-08 16:56:07 -0300

    projeto atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import {v4} from 'uuid';
 
 function App() {
-  const [tasks, setTasks] = useState([{
-    id: 1,
-    title: "Estudar programação",
-    description: "Estudar programação para desenvolver.",
-    isCompleted: false
-    }, 
-    {
-    id: 2,
-    title: "Estudar Inglês",
-    description: "Estudar ingês para se tornar fluente.",
-    isCompleted: false
-    }, 
-    {
-    id: 3,
-    title: "Estudar matemática",
-    description: "Estudar matemática para se tornar desenvolvedor",
-    isCompleted: false
-    },
-  ])
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async() => {
+      
+      //CHAMAR A API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10", 
+        {
+          method: 'GET'
+        }
+      );
+      
+      //PEGAR OS DADOS QUE ELA RETORNA
+      const data = await response.json();
+      
+      //ARMAZENAR/PERSISTIR ESSES DADOS NO STATE
+      setTasks(data);
+    }
+    
+    //SE QUISER, PODE PEGAR UMA API PARA PEGAR AS TAREFAS
+    //fetchTasks();
+  }, [])
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map(task => {
@@ -36,12 +47,32 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onDeleteTaskClick (taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks (newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title, 
+      description, 
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
-        <AddTask />
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks 
+          tasks={tasks} 
+          onTaskClick={onTaskClick} 
+          onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
   );
