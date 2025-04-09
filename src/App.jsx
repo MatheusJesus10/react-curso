2b362d46510d9927d804f7743356a524cbd76b43
Matheus Jesus <MatheusJesusJesus07@hotmail.com>
2025-04-09 09:18:08 -0300

    commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import React, { useEffect, useState } from 'react';
 import {v4} from 'uuid';
+import Title from "./components/Title";
 
 function App() {
   const [tasks, setTasks] = useState(
@@ -65,9 +66,9 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
-          Gerenciador de Tarefas
-        </h1>
+        
+        <Title>Gerenciador de Tarefas</Title>
+    
         <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
         <Tasks 
           tasks={tasks} 
